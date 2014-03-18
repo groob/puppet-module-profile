8a057a37da22db7a0f316dffa35998b3e443c2fb
groob <vrancean+github@gmail.com>
2014-03-17 23:10:00 -0400

    added etc/sysconfig/logstash file

--- manifests/logstash.pp
+++ manifests/logstash.pp
@@ -5,7 +5,8 @@ class profile::logstash {
   include ::java
   ensure_packages(['wget'])
   class { '::logstash':
-    package_url =>  'http://download.elasticsearch.org/logstash/logstash/packages/centos/logstash-1.3.3-1_centos.noarch.rpm'
+    package_url        => 'http://download.elasticsearch.org/logstash/logstash/packages/centos/logstash-1.3.3-1_centos.noarch.rpm',
+    init_defaults_file => 'puppet:///modules/profile/logstash/init_defaults_file',
   }
   logstash::configfile {'indexer':
     content => template('profile/logstash/indexer.erb'),
